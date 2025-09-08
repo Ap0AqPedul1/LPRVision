a5b604a17856818fb4d1ecd834365c44bd6593da
Ap0AqPedul1 <168333841+Ap0AqPedul1@users.noreply.github.com>
2025-09-09 00:29:19 +0700

    update

--- config.py
+++
@@ -1,7 +0,0 @@
-plat_nomor_indonesia = [
-    "A", "B", "D", "E", "F", "G", "H", "I", "J", "K", 
-    "L", "M", "N", "P", "R", "S", "T", "Z", "AB", "BB", 
-    "BD", "BG", "BH", "BK", "BL", "BM", "BN", "BP", "BR", 
-    "BS", "BT"
-]
-predictions = 'predictions'
